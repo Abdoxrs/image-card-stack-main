bd32c36fcc21f8027c08d148c0b5d7b5ee3adf23
AbdoX <abdoxrs223@gmail.com>
2025-12-11 22:37:31 +0200

    Adding Title

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { ImageCardStack } from "./components/features/ImageStack";
 
 export default function App() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <ImageCardStack />
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <h1 className="text-3xl font-extrabold text-center mb-15">Image-Card Slider</h1>
+      <div>
+        <ImageCardStack />
+      </div>
     </div>
   );
 }
